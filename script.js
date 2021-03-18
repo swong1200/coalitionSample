565d3f2c8047faa4571c1d5f9945726c29750c5b
Stuart Wong <swong31@gmail.com>
2021-03-17 22:03:21 -0700

    Fixed layout issue inside history section

---
+++ script.js
@@ -0,0 +1,11 @@
+$(document).ready(() => {
+  const mountain1 = $("#mountain1");
+  const mountain2 = $("#mountain2");
+
+  const schedule = $(".schedule");
+  const schedule2 = $(".schedule2");
+
+  mountain2.click(() => {
+    console.log("hello");
+  });
+});
